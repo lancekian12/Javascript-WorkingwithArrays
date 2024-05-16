732d5c530c8ac0f9795207cc631216c155dbc780
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-05-17 00:36:41 +0800

    started bankist project, added displaying dynamic transaction

--- script.js
+++ script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('beforeend', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -136,20 +154,20 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /// ... until it reaches the end of the array
 
 // MAP
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-currencies.forEach(function (currency, index, map) {
-  console.log(`${index}: ${currency}`);
-});
+// currencies.forEach(function (currency, index, map) {
+//   console.log(`${index}: ${currency}`);
+// });
 
-// Set
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique);
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
 
-currenciesUnique.forEach(function (value, key, set) {
-  console.log(`${key}: ${value}`);
-});
+// currenciesUnique.forEach(function (value, key, set) {
+//   console.log(`${key}: ${value}`);
+// });
