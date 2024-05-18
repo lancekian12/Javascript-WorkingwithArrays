a468b5864ef354d113bc410d1828ef2d0fb3c7c8
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-05-18 14:20:17 +0800

    Learned about find method

--- script.js
+++ script.js
@@ -101,7 +101,6 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * 1.2) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
       return int > 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -367,3 +366,21 @@ GOOD LUCK 😀
 //   .reduce((acc, mov) => acc + mov, 0);
 
 // console.log(totalDepositsUSD);
+
+// const calcAverageHumanAge = function (ages) {
+//   const dogAge = ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(dog => dog >= 18 && dog)
+//     .reduce((acc, dog, i, arr) => acc + dog / arr.length, 0);
+//   return dogAge;
+// };
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
